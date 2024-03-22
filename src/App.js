09536497eaf6f4cbecee14795c310afceedf1fa4
yungshun317 <seraph317@gmail.com>
2024-03-22 16:03:34 +0800

    [FEAT] Fix prop drilling with composition

--- src/App.js
+++ src/App.js
@@ -55,18 +55,25 @@ export default function App() {
 
   return (
       <>
-        <NavBar movies={movies} />
-        <Main movies={movies} />
+        <NavBar>
+            <Search />
+            <NumResults movies={movies} />
+        </NavBar>
+        <Main>
+            <ListBox>
+                <MovieList movies={movies} />
+            </ListBox>
+            <WatchedBox />
+        </Main>
       </>
   );
 }
 
-function NavBar({ movies }) {
+function NavBar({ children }) {
   return (
       <nav className="nav-bar">
-        <Logo />
-        <Search />
-        <NumResults movies={movies} />
+          <Logo />
+          {children}
       </nav>
   );
 }
@@ -102,14 +109,13 @@ function Search() {
   );
 }
 
-function Main({ movies }) {
+function Main({ children }) {
   return <main className="main">
-    <ListBox movies={movies} />
-    <WatchedBox />
+      {children}
   </main>
 }
 
-function ListBox({ movies }) {
+function ListBox({ children }) {
   const [isOpen1, setIsOpen1] = useState(true);
 
   return (
@@ -120,7 +126,7 @@ function ListBox({ movies }) {
         >
           {isOpen1 ? "–" : "+"}
         </button>
-        {isOpen1 && <MovieList movies={movies} />}
+        {isOpen1 && children}
       </div>
   );
 }
