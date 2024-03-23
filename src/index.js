9527e127b3e865a9798a48e0eb9e3a0dc2b7eb61
yungshun317 <seraph317@gmail.com>
2024-03-23 18:07:10 +0800

    [FEAT] Improve reusability with props and give the outside component the ability to get access to the internal state inside the `StarRating` component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import { useState } from "react";
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './App';
 import StarRating from "./StarRating";
 
+function Test() {
+    const [movieRating, setMovieRating] = useState(0);
+
+    return (
+        <div>
+            <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+            <p>This movie was rated {movieRating}</p>
+        </div>
+    );
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
       {/* <App /> */}
-      <StarRating maxRating={5} />
-      <StarRating maxRating={10} />
-      <StarRating />
+      <StarRating maxRating={5} messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']} />
+      <StarRating size={24} color="red" className="test" />
+      <StarRating defaultRating={3} />
+      <Test />
   </React.StrictMode>
 );
