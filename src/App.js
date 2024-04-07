300fd4f68600e75eb1e93ad87f580dcb45176249
yungshun317 <seraph317@gmail.com>
2024-04-07 19:58:11 +0800

    [FEAT] Close the `MovieDetails` component if the movie has been selected

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ export default function App() {
   */
 
   function handleSelectMovie(id) {
-      setSelectedId(id);
+      setSelectedId((selectedId) => (id === selectedId ? null : id));
   }
 
   function handleCloseMovie() {
