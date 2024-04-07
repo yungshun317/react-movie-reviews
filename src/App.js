64e7acc0763e77defa014c5cba9b1dea5902254a
yungshun317 <seraph317@gmail.com>
2024-04-07 20:27:34 +0800

    [FEAT] Display the `Loader` component while loading movie details

--- src/App.js
+++ src/App.js
@@ -290,6 +290,7 @@ function Movie({ movie, onSelectMovie }) {
 
 function MovieDetails({ selectedId, onCloseMovie }) {
     const [movie, setMovie] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
 
     const {
         Title: title,
@@ -306,11 +307,13 @@ function MovieDetails({ selectedId, onCloseMovie }) {
 
     useEffect(function() {
         async function getMovieDetails() {
+            setIsLoading(true);
             const res = await fetch(
                 `http://www.omdbapi.com/?apikey=${API_KEY}&i=${selectedId}`
             );
             const data = await res.json();
             setMovie(data);
+            setIsLoading(false);
 
             // console.log(data);
             /*
@@ -344,37 +347,41 @@ function MovieDetails({ selectedId, onCloseMovie }) {
             */
         }
         getMovieDetails();
-    }, []);
+    }, [selectedId]);
 
     return (
         <div className="details">
-            <header>
-                <button className="btn-back" onClick={onCloseMovie}>
-                    &larr;
-                </button>
-                <img src={poster} alt={`Poster of ${movie} movie`} />
-                <div className="details-overview">
-                    <h2>{title}</h2>
-                    <p>
-                        {released} &bull; {runtime}
-                    </p>
-                    <p>{genre}</p>
-                    <p>
-                        <span>⭐️</span>
-                        {imdbRating} IMDb rating
-                    </p>
-                </div>
-            </header>
-            <section>
-                <div className="rating">
-                    <StarRating maxRating={10} size={24} />
-                </div>
-                <p>
-                    <em>{plot}</em>
-                </p>
-                <p>Starring {actors}</p>
-                <p>Directed by {director}</p>
-            </section>
+            {isLoading ? <Loader /> :
+                <>
+                    <header>
+                        <button className="btn-back" onClick={onCloseMovie}>
+                            &larr;
+                        </button>
+                        <img src={poster} alt={`Poster of ${movie} movie`} />
+                        <div className="details-overview">
+                            <h2>{title}</h2>
+                            <p>
+                                {released} &bull; {runtime}
+                            </p>
+                            <p>{genre}</p>
+                            <p>
+                                <span>⭐️</span>
+                                {imdbRating} IMDb rating
+                            </p>
+                        </div>
+                    </header>
+                    <section>
+                        <div className="rating">
+                            <StarRating maxRating={10} size={24} />
+                        </div>
+                        <p>
+                            <em>{plot}</em>
+                        </p>
+                        <p>Starring {actors}</p>
+                        <p>Directed by {director}</p>
+                    </section>
+                </>
+            }
             {selectedId}
         </div>
     );
