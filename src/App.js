eb08ac2afe59d8cb310a43d679fcb6cc1468634a
yungshun317 <seraph317@gmail.com>
2024-04-07 21:12:52 +0800

    [FEAT] Inform users if they watched the movie or not in `MovieDetails`

--- src/App.js
+++ src/App.js
@@ -173,6 +173,7 @@ export default function App() {
                             selectedId={selectedId}
                             onCloseMovie={handleCloseMovie}
                             onAddWatched={handleAddWatched}
+                            watched={watched}
                         /> :
                         <>
                             <WatchedSummary watched={watched} />
@@ -296,10 +297,13 @@ function Movie({ movie, onSelectMovie }) {
   );
 }
 
-function MovieDetails({ selectedId, onCloseMovie, onAddWatched }) {
+function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     const [movie, setMovie] = useState({});
     const [isLoading, setIsLoading] = useState(false);
     const [userRating, setUserRating] = useState("");
+
+    const isWatched = watched.map(movie => movie.imdbID).includes(selectedId);
+    const watchedUserRating = watched.find(movie => movie.imdbID === selectedId)?.userRating;
 
     const {
         Title: title,
@@ -395,21 +399,28 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched }) {
                     </header>
                     <section>
                         <div className="rating">
-                            <StarRating
-                                maxRating={10}
-                                size={24}
-                                onSetRating={setUserRating}
-                            />
                             {
-                                userRating > 0 &&
-                                <button className="btn-add" onClick={handleAdd}>+ Add to List</button>
+                                !isWatched ?
+                                    <>
+                                        <StarRating
+                                            maxRating={10}
+                                            size={24}
+                                            onSetRating={setUserRating}
+                                        />
+                                        {
+                                            userRating > 0 &&
+                                            <button className="btn-add" onClick={handleAdd}>+ Add to List</button>
+                                        }
+                                    </>
+                                     :
+                                <p>You Rated this Movie {watchedUserRating}</p>
                             }
                         </div>
                         <p>
                             <em>{plot}</em>
                         </p>
                         <p>Starring {actors}</p>
-                        <p>Directed by {director}</p>
+                        <p>Directed by {director} <span>⭐️</span></p>
                     </section>
                 </>
             }
