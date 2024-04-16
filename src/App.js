7c0cea5d4c979d682a740caffbb2a75210453dda
yungshun317 <seraph317@gmail.com>
2024-04-16 23:00:03 +0800

    [FEAT] Persist data between renders using refs

--- src/App.js
+++ src/App.js
@@ -358,6 +358,13 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     const [isLoading, setIsLoading] = useState(false);
     const [userRating, setUserRating] = useState("");
 
+    const countRef = useRef(0);
+
+    // Check states including `countRatingDecisions` in `hooks` of `App` in the `Components` tab of the React Developer Tools
+    useEffect(function() {
+        if (userRating) countRef.current = countRef.current + 1;
+    }, [userRating]);
+
     const isWatched = watched.map(movie => movie.imdbID).includes(selectedId);
     const watchedUserRating = watched.find(movie => movie.imdbID === selectedId)?.userRating;
 
@@ -405,7 +412,8 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
             poster,
             imdbRating: Number(imdbRating),
             runtime: Number(runtime.split(" ").at(0)),
-            userRating
+            userRating,
+            countRatingDecisions: countRef.current
         }
         onAddWatched(newWatchedMovie);
         onCloseMovie();
